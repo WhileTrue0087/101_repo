8c55f51d42eb6b3e5a611c1b121beb3cc67ad93f
MohammadTaha <136187510+WhileTrue0087@users.noreply.github.com>
2024-02-03 10:37:15 +0330

    Add files via upload
    پیدا کردن عدد اول

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//let n = 100000007;
+//let isPrime = true;
+//let i = 2;
+//while (i < parseInt(n ** 0.5) + 1) {
+//    if (n % i == 0) {
+// isPrime = false;
+//   console.log(i);
+//     break;
+//   }
+//  i++;
+//}
+//console.log(n, isPrime);
